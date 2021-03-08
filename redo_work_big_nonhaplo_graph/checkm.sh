414f1d0f0254d06e7513932849176a3e0a08cb2d
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2021-03-08 08:21:19 -0800

    add checkM runner; small updates

---
+++ redo_work_big_nonhaplo_graph/checkm.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+# The purpose of running CheckM is to get a sense of edge sequence quality --
+# e.g. are any really obvious chimeras, etc.
+
+# Use of lineage_wf based on
+# https://github.com/Ecogenomics/CheckM/wiki/Quick-Start
+# -- may be overkill since I really just want QA, but ok with me.
+#
+# Also, use of -t 10 based on Misha's use of CheckM on the sheep gut dataset.
+checkm lineage_wf -t 10 edges/ checkm_edges_output/
--- redo_work_big_nonhaplo_graph/get_seqs_from_fasta.py
+++ redo_work_big_nonhaplo_graph/get_seqs_from_fasta.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# NOTE: uh coming back to this, grep "seq_name$" -A 1 > seq_name.fasta
+# also totally works lmao (the $ avoids containments -- e.g. edge_1
+# will match both >edge_1 and also >edge_10 and >edge_11, etc.)
+
 from collections import defaultdict
 
 FASTALOC = "/Poppy/mkolmogo/sheep_meta/flye_big_2.8/assembly.fasta"
