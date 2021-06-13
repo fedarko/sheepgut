5f124ffe409df8d391b9a8ed7a3dab8f23a2bb72
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2021-06-12 19:35:40 -0700

    run ntbks script docs

--- notebooks/RUN-NOTEBOOKS.py
+++ notebooks/RUN-NOTEBOOKS.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python3
 # From https://github.com/biocore/qurro/blob/master/Makefile
 # (which in turn was from https://nbconvert.readthedocs.io/en/latest/usage.html#notebook-and-preprocessors)
+# NOTE: currently this just runs the notebooks in sorted filename order, which
+# is good enough. Ideally we'd set this up using Snakemake / etc., since some
+# notebooks rely on others having already been run
+# (so far the only instances of this I can remember are
+# LinkedMutations-ClassifyReads before LinkedMutations-CallAndPlot, and
+# MutationMatrices before PlotMutationMatrices).
+
 
 import os
 import subprocess
@@ -8,7 +15,6 @@ import subprocess
 # Notebooks we don't (explicitly) run here. This includes:
 # -Notebooks that exist only to be run by other notebooks
 #  (e.g. header, gene utils, load mutation JSON data)
-#  (e.g. codon/aa mutation matrices, linked mutation read classification)
 # -Notebooks that rely on Graphviz' sfdp utility and the PRISM overlap
 #  removal method -- which in turn relies on the "gts" library -- which I
 #  have not been able to get installed on our cluster yet, so I've just been
@@ -21,7 +27,8 @@ EXCLUDED_NOTEBOOKS = [
 ]
 
 cwd_files = os.listdir()
-for f in cwd_files:
+# Make order deterministic by sorting
+for f in sorted(cwd_files):
     if f.endswith(".ipynb"):
         if f not in EXCLUDED_NOTEBOOKS:
             print("Running notebook {}".format(f))
