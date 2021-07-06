ad9c4682860e4192a29d2e5ac6c62b2c3058dc1f
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2021-07-05 18:46:18 -0700

    linked mutation classification; new pileup, naive

--- notebooks/linked_mutations_utils.py
+++ notebooks/linked_mutations_utils.py
@@ -1,3 +1,5 @@
+import pileup
+
 # This file defines various constants / functions used in the
 # linked mutation analysis notebooks.
 
@@ -31,15 +33,11 @@ def emptyListOf4():
 
 
 # Finds all mutated positions in a genome. Stored here to enable reuse.
-# "seq2pos2matchct", etc are the same as they are in the mutation JSON
-# notebook
-def find_mutated_positions(seq, seq2pos2matchct, seq2pos2mismatchct):
+def find_mutated_positions(seq):
+    seq2pos2pileup = pileup.load()
     mutated_positions = []
-    for pos in seq2pos2matchct[seq].keys():
-        matchct = seq2pos2matchct[seq][pos]
-        mismatchct = seq2pos2mismatchct[seq][pos]
-        cov = mismatchct + matchct
-        
+    for pos, pcol in enumerate(seq2pos2pileup[seq][1:], 1):
+        cov = pileup.get_cov(pcol)
         # We can be strict and filter out positions that don't pass the
         # coverage filter for linked reads -- no sense including these.
         if cov >= MIN_COV_OF_MUTATIONS_AT_LINKED_POSITIONS:
@@ -47,9 +45,9 @@ def find_mutated_positions(seq, seq2pos2matchct, seq2pos2mismatchct):
             # Actually "call" mutations, the same way we do elsewhere in
             # these analyses (albeit maybe with different values of MINFREQ).
             # Of course, this isn't the only way to do this.
-            if (mismatchct / cov) > MINFREQ:
+            if pileup.naively_call_mutation(pcol, MINFREQ):
                 # We need 0-indexed positions so we can work with skbio /
-                # pysam. We therefore decrease these by 1, since the JSONs are
-                # 1-indexed.
-                mutated_positions.append(int(pos) - 1)
+                # pysam. We therefore decrease these by 1, since the pileup
+                # positions are 1-indexed.
+                mutated_positions.append(pos - 1)
     return mutated_positions
