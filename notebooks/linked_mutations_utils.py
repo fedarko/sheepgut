b949842b5b548b0cd193fbcfe42ea8da3e45f9fd
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2021-10-11 17:55:27 -0700

    Update mutation calling in LM utils, for phasing prep

--- notebooks/linked_mutations_utils.py
+++ notebooks/linked_mutations_utils.py
@@ -3,34 +3,18 @@ import pileup
 # This file defines various constants / functions used in the
 # linked mutation analysis notebooks.
 
-# unless (pos j) - (pos i) < this, we do not consider i and j linked.
-MAX_DIST_BTWN_LINKED_POSITIONS_NONINCLUSIVE = float("inf")
+# We only consider a position if it has at least this much coverage. This is a
+# very greedy thing to do, but thanks to super deep coverage in these genomes
+# it's feasible.
+MINCOV = 1000
 
+# In order for us to connect two allele nodes, at least this many reads must
+# span both allele's positions (this isn't the only condition for creating an
+# edge; the two alleles must also co-occur on at least one read).
+MINSPAN = 500
 
-# unless at least this many reads have mutations at both pos i and pos j,
-# we do not consider i and j linked.
-MIN_COV_OF_MUTATIONS_AT_LINKED_POSITIONS = 1000
-
-
-# unless |Reads(i, -)| + |Reads(-, j)| < this fraction * |Reads(i, j)|,
-# we do not consider i and j linked.
-MAX_NONLINKED_MUTATED_FRACTION_NONINCLUSIVE = 0.2
-
-
-# How we call a mutation: only if
-# (# mismatches) / (# mismatches + # matches) > MINFREQ.
-# Defaults to 0.5%.
-MINFREQ = 0.005
-
-
-# Used to initialize entries in the pospair2groupcts defaultdicts.
-# This was originally a lambda function, but that breaks pickle:
-# https://stackoverflow.com/a/16439720
-# And it looks like it needs to be in scope when loading the pickled
-# defaultdicts, anyway. So we use an ordinary function here instead.
-def emptyListOf4():
-    return [0, 0, 0, 0]
-
+# Value of p used for calling a position as "mutated" or not. defaults to 0.5%
+p = 0.5 / 100
 
 # Finds all mutated positions in a genome. Stored here to enable reuse.
 def find_mutated_positions(seq):
@@ -40,12 +24,8 @@ def find_mutated_positions(seq):
         cov = pileup.get_cov(pcol)
         # We can be strict and filter out positions that don't pass the
         # coverage filter for linked reads -- no sense including these.
-        if cov >= MIN_COV_OF_MUTATIONS_AT_LINKED_POSITIONS:
-            
-            # Actually "call" mutations, the same way we do elsewhere in
-            # these analyses (albeit maybe with different values of MINFREQ).
-            # Of course, this isn't the only way to do this.
-            if pileup.naively_call_mutation(pcol, MINFREQ):
+        if cov >= MINCOV:
+            if pileup.naively_call_mutation(pcol, p):
                 # We need 0-indexed positions so we can work with skbio /
                 # pysam. We therefore decrease these by 1, since the pileup
                 # positions are 1-indexed.
