339a7d0e517ff54ed4b59cdbbe095622a36222d4
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2021-10-23 20:24:05 -0700

    Tidy up VCF writing into real script, & fix ref nt

--- notebooks/write-naive-vcf.py
+++ notebooks/write-naive-vcf.py
@@ -1,61 +1,37 @@
+import time
 import pileup
-spp = pileup.load()
-ls
-spp["edge_6104"][27553]
-spp["edge_6104"][27552]
-spp["edge_6104"][27554]
-13 / (4925 + 13)
-25 / (4912 + 25)
-0.5 / 100
-vcf = ""
-seq2mp = defaultdict(list)
-for seq in SEQS:
-    for pos, pcol in enumerate(spp[seq][1:], 1):
-        cov = pileup.get_cov(pcol)
-        if cov >= 1000:
-            if pileup.naively_call_mutation(pcol, 0.005):
-                # vcf positions are 1-indexed, same as pileup stuff
-                seq2mp[seq].append(pos)
-                # NOTE this may wind up having ref == alt; doesn't matter much r/n but can change if needed
-                ref_nt = "ACGT"[pcol[1]]
-                alt_nt = pileup.get_alt_nt(pcol)
-                vcf += f"{seq}\t{pos}\t.\t{ref_nt}\t{alt_nt}\t.\t.\t.\n"
 from collections import defaultdict
-vcf = ""
-seq2mp = defaultdict(list)
+
+# Stuff included elsewhere and duplicated here (sorry!)
+SEQS = ["edge_6104", "edge_1671", "edge_2358"]
+n2i = {"A": 0, "C": 1, "G": 2, "T": 3}
+i2n = "ACGT"
+
+spp = pileup.load()
+# Header info gleaned by reading over the VCF 4.2 docs
+# (https://samtools.github.io/hts-specs/VCFv4.2.pdf) and copying how LoFreq
+# organizes their header
+vcf = (
+    "##fileformat=VCFv4.2\n"
+    f"##fileDate={time.strftime('%Y%m%d')}\n"
+    # TODO include version number in the source, as shown in VCF 4.2 docs?
+    "##source=strainFlye\n"
+    "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n"
+)
 for seq in SEQS:
     for pos, pcol in enumerate(spp[seq][1:], 1):
         cov = pileup.get_cov(pcol)
         if cov >= 1000:
             if pileup.naively_call_mutation(pcol, 0.005):
                 # vcf positions are 1-indexed, same as pileup stuff
-                seq2mp[seq].append(pos)
-                # NOTE this may wind up having ref == alt; doesn't matter much r/n but can change if needed
-                ref_nt = "ACGT"[pcol[1]]
                 alt_nt = pileup.get_alt_nt(pcol)
+                # extremely silly hack to get the reference nt
+                # this might not be necessary but this should consistently
+                # at least set it to something that is not the alt nt
+                ctscopy = pcol[0][:]
+                ctscopy[n2i[alt_nt]] = float("-inf")
+                ref_nt = max("ACGT", key=lambda nt: ctscopy[n2i[nt]])
                 vcf += f"{seq}\t{pos}\t.\t{ref_nt}\t{alt_nt}\t.\t.\t.\n"
-%run "Header.ipynb"
-vcf = ""
-seq2mp = defaultdict(list)
-for seq in SEQS:
-    for pos, pcol in enumerate(spp[seq][1:], 1):
-        cov = pileup.get_cov(pcol)
-        if cov >= 1000:
-            if pileup.naively_call_mutation(pcol, 0.005):
-                # vcf positions are 1-indexed, same as pileup stuff
-                seq2mp[seq].append(pos)
-                # NOTE this may wind up having ref == alt; doesn't matter much r/n but can change if needed
-                ref_nt = "ACGT"[pcol[1]]
-                alt_nt = pileup.get_alt_nt(pcol)
-                vcf += f"{seq}\t{pos}\t.\t{ref_nt}\t{alt_nt}\t.\t.\t.\n"
-vcf
-vcf.split("\n")[0]
-print(_)
-spp["edge_6104"][1596]
-25 / (25+4842)
+
 with open("../seqs/naive_mincov1000_p0.5pct.vcf", "w") as vf:
     vf.write(vcf)
-ls
-ls ../seqs
-ls
-%history -f write-naive-vcf.py
