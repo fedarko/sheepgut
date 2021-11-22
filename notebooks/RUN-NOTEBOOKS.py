5943b249fee5c58d6a92c3906eaf1f33b96e39fa
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2021-11-22 04:32:23 -0800

    run ntbks update
    just need to jury rig this to get phasing done...
    
    aosidjoidsjfoisdjfoiajdfoijfdoij someone call amy schumer because
    this week is a train wreck :D

--- notebooks/RUN-NOTEBOOKS.py
+++ notebooks/RUN-NOTEBOOKS.py
@@ -3,33 +3,22 @@
 # (which in turn was from https://nbconvert.readthedocs.io/en/latest/usage.html#notebook-and-preprocessors)
 # NOTE: currently this just runs the notebooks in sorted filename order, which
 # is good enough. Ideally we'd set this up using Snakemake / etc., since some
-# notebooks rely on others having already been run
-# (so far the only instances of this I can remember are
-# LinkedMutations-ClassifyReads before LinkedMutations-CallAndPlot, and
-# MutationMatrices before PlotMutationMatrices).
+# notebooks rely on others having already been run (e.g. matrices, phasing).
 
 
 import os
 import subprocess
 
-# Notebooks we don't (explicitly) run here. This includes:
-# -Notebooks that exist only to be run by other notebooks
-#  (e.g. header, gene utils)
-# -Notebooks that rely on Graphviz' sfdp utility and the PRISM overlap
-#  removal method -- which in turn relies on the "gts" library -- which I
-#  have not been able to get installed on our cluster yet, so I've just been
-#  running this notebook on my laptop for now (linked mutation call&plot)
 EXCLUDED_NOTEBOOKS = [
     "Header.ipynb",
     "GeneUtils.ipynb",
-    "LinkedMutations-CallAndPlot.ipynb",
 ]
 
 cwd_files = os.listdir()
 # Make order deterministic by sorting
 for f in sorted(cwd_files):
     if f.endswith(".ipynb"):
-        if f not in EXCLUDED_NOTEBOOKS:
+        if f.startswith("Phasing-"):
             print("Running notebook {}".format(f))
             subprocess.run([
                 "jupyter",
