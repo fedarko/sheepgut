e79986e1cb9761b296b3809cfb052b30b4c062a1
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2022-09-20 20:24:58 -0700

    Work on FDR curve ntbk, etc
    ultimately drafting; can adjust input path when p=0.15% call done

---
+++ sf-analyses/sheep/2-fdr-estimate.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+#
+# Performs mutation calling using the actual strainFlye pipeline code (not the
+# messy ad hoc code used in notebooks/, for example).
+#
+# This enables us to use some of the more recently developed strainFlye
+# functionality on the SheepGut dataset. For example, drawing some recently
+# implemented types of FDR curves, or computing a p-value for the longest gap
+# between mutations in a contig.
+
+CONTIGS=/Poppy/mfedarko/sheepgut/main-workflow/output/all_edges.fasta
+BAM=/Poppy/mfedarko/sheepgut/main-workflow/output/fully-filtered-and-sorted-aln.bam
+
+strainFlye fdr estimate
+    --contigs $CONTIGS \
+    --bam $BAM \
+    --bcf output/call-p15/naive-calls.bcf \
+    --decoy-contig "edge_6104" \
+    --decoy-contexts "Everything" \
+    --output-dir output/fdr-estimate
